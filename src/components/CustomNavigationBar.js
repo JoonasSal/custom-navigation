3700b9f79232762a4e40a20a90c11de3ec81a2a7
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-09-08 22:26:01 +0300

    no nyt

--- src/components/CustomNavigationBar.js
+++ src/components/CustomNavigationBar.js
@@ -1,20 +1,55 @@
 import React from 'react';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Menu } from 'react-native-paper';
 import { getHeaderTitle } from '@react-navigation/elements';
 
-const CustomNavigationBar = ({ navigation, route, options, back }) => {
+export default function CustomNavigationBar({
+  navigation,
+  route,
+  options,
+  back,
+}) {
+  // Tilat valikon näkyvyyden hallintaan
+  const [visible, setVisible] = React.useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
   const title = getHeaderTitle(options, route.name);
 
   return (
     <Appbar.Header>
-      {back ? (
-        <Appbar.BackAction onPress={navigation.goBack} />
-      ) : (
-        <Appbar.Action icon="arrow-right" onPress={() => navigation.navigate('SecondScreen')} />
-      )}
+      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
       <Appbar.Content title={title} />
+      {!back ? (
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={
+            <Appbar.Action
+              icon="dots-vertical"
+              onPress={openMenu}
+            />
+          }>
+          <Menu.Item
+            onPress={() => {
+              console.log('Option 1 was pressed');
+            }}
+            title="Option 1"
+          />
+          <Menu.Item
+            onPress={() => {
+              console.log('Option 2 was pressed');
+            }}
+            title="Option 2"
+          />
+          <Menu.Item
+            onPress={() => {
+              console.log('Option 3 was pressed');
+            }}
+            title="Option 3"
+            disabled
+          />
+        </Menu>
+      ) : null}
     </Appbar.Header>
   );
-};
-
-export default CustomNavigationBar;
+}
